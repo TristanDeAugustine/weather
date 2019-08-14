37c0504c8efff4426fece55744460c5a6107f273
Tristan <deaugustinetristan@gmail.com>
2019-08-13 21:11:19 -0400

    api

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let city = document.querySelector('.search')
+
+const createSearch = () => {
+  let typedName = city.value
+  let search = typedName
+  const url = 'http://api.openweathermap.org/data/2.5/weather'
+  const apiKey = '74652b269116515a06484190cbdffe93'
+  const newUrl = url + '?' + 'q=' + search + '&appid=' + apiKey
+  return newUrl
+}
+const weatherAndTemp = () => {
+  fetch(createSearch())
+    .then(response => {
+      return response.json()
+    })
+    .then(city => {
+      document.querySelector('.weather').textContent = city.weather[0].main
+
+      document.querySelector('.temperature').textContent = city.main.temp
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.button').addEventListener('click', weatherAndTemp)
